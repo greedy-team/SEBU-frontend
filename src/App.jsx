de1c54ade43c582875c6ab9915a580300ed2e272
gyuminJJANG <ggavwu789@gmail.com>
2026-08-16 17:30:31 +0900

    feat: 로그인 및 마이페이지 화면 생성 및 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/Search";
 import CollegeView from "./pages/CollegeView";
+import LoginPage from "./pages/Login";
+import MyPageSetup from "./pages/MyPageSetup";
+
 function App() {
   return (
     <Routes>
       {" "}
       <Route path="/" element={<SearchPage />} />{" "}
       <Route path="/colleges" element={<CollegeView />} />{" "}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/mypage-setup" element={<MyPageSetup />} />
     </Routes>
   );
 }
