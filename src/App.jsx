0677866ba05297fc5918629e941d54896828cd8a
rahwan10 <donggeon10@naver.com>
2026-09-06 16:18:37 +0900

    fix: conflict해결하며 수정된 토스트 다시 재생성

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,24 @@ import RateLimitToast from "./components/common/RateLimitToast";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SearchPage />} />
-      <Route path="/colleges" element={<CollegeView />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/design-system" element={<DesignSystem />} />
-      <Route path="/community" element={<CommunityPage />} />
-      <Route path="/community/write" element={<PostWritePage />} />
-      <Route path="/community/labs/:laboratoryId" element={<LabReviewPage />} />
-      <Route path="/community/:postId/edit" element={<PostWritePage />} />
-      <Route path="/community/:postId" element={<PostDetailPage />} />
-    </Routes>
+    <>
+      <RateLimitToast />
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/colleges" element={<CollegeView />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/design-system" element={<DesignSystem />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/community/write" element={<PostWritePage />} />
+        <Route
+          path="/community/labs/:laboratoryId"
+          element={<LabReviewPage />}
+        />
+        <Route path="/community/:postId/edit" element={<PostWritePage />} />
+        <Route path="/community/:postId" element={<PostDetailPage />} />
+      </Routes>
+    </>
   );
 }
 
