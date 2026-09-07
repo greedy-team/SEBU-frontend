1298467d81a61ea9195b081ca64720fa6a08c57e
rahwan10 <donggeon10@naver.com>
2026-09-07 16:22:34 +0900

    fix: 라우팅 롤백

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import CommunityPage from "./pages/Community";
 import PostDetailPage from "./pages/PostDetail";
 import PostWritePage from "./pages/PostWrite";
 import RateLimitToast from "./components/common/RateLimitToast";
+import LabReviewHomePage from "./pages/LabReviewHome";
+import LabReviewPage from "./pages/LabReview";
+import LabReviewWritePage from "./pages/LabReviewWrite";
 import { useAuthRestore } from "./features/auth/hooks/useAuthRestore";
 
 function App() {
@@ -25,6 +28,16 @@ function App() {
         <Route path="/community" element={<CommunityPage />} />
         <Route path="/community/write" element={<PostWritePage />} />
         <Route path="/community/:postId" element={<PostDetailPage />} />
+        <Route path="/community/labs" element={<LabReviewHomePage />} />
+        <Route
+          path="/community/labs/:laboratoryId/write"
+          element={<LabReviewWritePage />}
+        />
+        <Route
+          path="/community/labs/:laboratoryId"
+          element={<LabReviewPage />}
+        />
+        <Route path="/community/:postId/edit" element={<PostWritePage />} />
       </Routes>
     </>
   );
