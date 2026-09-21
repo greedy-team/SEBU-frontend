0bbcba44d622ff2a4f506f4c7f1e85df90811651
rahwan10 <donggeon10@naver.com>
2026-09-21 15:06:37 +0900

    test: 렌더링 테스트

--- src/features/search/components/LabList.jsx
+++ src/features/search/components/LabList.jsx
@@ -11,7 +11,7 @@ function LabList({ labs }) {
 
   return (
     <div className="flex flex-col gap-3">
-      {labs.map((lab) => (
+      {labs.slice(0, 10).map((lab) => (
         <LabCard key={lab.id} lab={lab} />
       ))}
     </div>
