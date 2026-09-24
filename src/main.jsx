f6e214173f5596c51373d37ce9701b46bc67ede8
rahwan10 <donggeon10@naver.com>
2026-09-24 13:35:58 +0900

    feat: QueryClient 설정 및 QueryClientProvider 적용

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./api/queryClient";
 
 async function enableMocking() {
   if (
@@ -18,9 +21,12 @@ async function enableMocking() {
 enableMocking().then(() => {
   createRoot(document.getElementById("root")).render(
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 });
