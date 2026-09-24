a3ea1703c086ede1841faf8aecab82f40ee75013
rahwan10 <donggeon10@naver.com>
2026-09-24 21:31:39 +0900

    fix: 로그인 사용자 변경 시 사용자별 캐시 자동 초기화

--- src/api/queryClient.js
+++ src/api/queryClient.js
@@ -1,5 +1,4 @@
 import { QueryClient } from "@tanstack/react-query";
-import { useAuthStore } from "../store/authStore";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -7,12 +6,4 @@ export const queryClient = new QueryClient({
       staleTime: 0,
     },
   },
-});
-console.log("queryClient.js 실행됨");
-useAuthStore.subscribe((state, prevState) => {
-  console.log("user 변경", prevState.user?.id, "→", state.user?.id);
-  if (state.user?.id === prevState.user?.id) return;
-
-  queryClient.removeQueries({ queryKey: ["mypage"] });
-  queryClient.invalidateQueries({ queryKey: ["laboratories"] });
 });
